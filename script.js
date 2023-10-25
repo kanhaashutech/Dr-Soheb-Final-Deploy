8a301f7019c2133ed21e27c7257d2a5c68fca3c7
kanhaashutech <kashuexpert@gmail.com>
2023-10-25 15:28:52 +0530

    commit responsive code

--- script.js
+++ script.js
@@ -8,5 +8,33 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-  //  results swiper
+  //  Toggle btn
+
+const hamburger = document.getElementById("menu_ashu");
+// const navMenu = document.getElementById("nav-menu");
+const nav_mobile_menu = document.querySelector(".acive");
+const navBtns = document.querySelector(".nav-btns");
+// const closeIcon = document.getElementById("nav-close");
+// const navLink = document.querySelectorAll(".nav__link");
+
+// navLink.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     navMenu.classList.add("hidden");
+//   })
+// );
+
+// closeIcon.addEventListener("click", () => {
+//   navMenu.classList.add("hidden");
+// });
+
+const toggleNavbar = () => {
+  navBtns.classList.toggle("active");
+  console.log((hello));
+}
+
+hamburger.addEventListener("click", () => toggleNavbar());
+// hamburger.addEventListener("click", () => {
+//   navMenu.classList.remove("hidden");
+// });
+
   
