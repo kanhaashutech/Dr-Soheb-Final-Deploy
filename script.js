d5bd2beda5a226072a19e73d1c09b199a0d4ee58
kanhaashutech <kashuexpert@gmail.com>
2023-10-25 22:13:06 +0530

    nav bar responsive

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // swiper code
 
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
-  //  Toggle btn
+//  Toggle btn
 
-const hamburger = document.getElementById("menu_ashu");
+// let x = document.getElementById("active");
+// let menuBtn = document.querySelector(".menuBtn");
+// let closeBtn = document.querySelector(".closeBtn");
+
+
 // const navMenu = document.getElementById("nav-menu");
-const nav_mobile_menu = document.querySelector(".acive");
-const navBtns = document.querySelector(".nav-btns");
+// const nav_mobile_menu = document.querySelector(".acive");
+// const navBtns = document.querySelector(".nav-btns");
 // const closeIcon = document.getElementById("nav-close");
 // const navLink = document.querySelectorAll(".nav__link");
 
@@ -27,14 +31,53 @@ const navBtns = document.querySelector(".nav-btns");
 //   navMenu.classList.add("hidden");
 // });
 
-const toggleNavbar = () => {
-  navBtns.classList.toggle("active");
-  console.log((hello));
+
+const x = document.getElementById('active');
+let menuBtn = document.querySelector(".menuBtn");
+let closeBtn = document.querySelector(".closeBtn");
+
+function openMenu() {
+
+  let menuBtn = document.querySelector(".menuBtn");
+  let closeBtn = document.querySelector(".closeBtn");
+
+  const x = document.getElementById('active');
+  x.style.display = 'flex';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+
+}
+function closeMenu() {
+
+
+  let menuBtn = document.querySelector(".menuBtn");
+  let closeBtn = document.querySelector(".closeBtn");
+  const x = document.getElementById('active');
+  
+  x.style.display = 'none';
+  menuBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
+
 }
 
-hamburger.addEventListener("click", () => toggleNavbar());
+
+
+// const toggleNavbar = () => {
+//   navBtns.classList.toggle("active");
+//   console.log((hello));
+// }
+
+// hamburger.addEventListener("click", () => toggleNavbar());
+
+
+
+
+
+
+
+
 // hamburger.addEventListener("click", () => {
 //   navMenu.classList.remove("hidden");
 // });
 
-  
+
